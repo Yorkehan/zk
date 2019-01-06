b015756da28801b02e2f70e9c457d27c61328aef
yorkehan <740081506@qq.com>
2019-01-06 21:46:09 +0800

    zookeeper

---
+++ src/main/java/com/yorkehan/zk/config/shiro/MyCredentialsMatcher.java
@@ -0,0 +1,6 @@
+package com.yorkehan.zk.config.shiro;
+
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+
+public class MyCredentialsMatcher extends HashedCredentialsMatcher {
+}
